b15f632452676bbaf2305856c01d989446b31da5
baudyvo20 <baudyvo20@sps-prosek.cz>
2023-04-27 16:05:15 +0200

    Initial commit

---
+++ KinoRezervejsnJednoduche/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MahApps.Metro.Controls;
+
+namespace kinoRezervejsnJednoduche
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : MetroWindow
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			// CinemaGenerator();
+			// Frame.NavigationService.Navigate(new MenuPage());
+		}
+	}
+}
---
+++ KinoRezervejsnJednoduche/Service/SaveManager.cs
@@ -0,0 +1,44 @@
+﻿using System.IO;
+using Newtonsoft.Json;
+
+namespace KinoRezervejsnJednoduche.Service;
+
+// Created by Vojts 
+// Github: https://github.com/vojtechbaudys
+// v1.1
+public class SaveManager
+{
+	// Save stats to {filePath.json} file
+	// filePath - [string]
+	// model - [object]
+	public void Save(string filePath, object model)
+	{
+		FileExist(filePath);
+		File.WriteAllText(filePath, JsonConvert.SerializeObject(model));
+	}
+	
+	// Load data form {save.json} file
+	// filePath - [string]
+	// Return T object
+	public T LoadData<T>(string filePath)
+	{
+		FileExist(filePath);
+		return JsonConvert.DeserializeObject<T>(File.ReadAllText(filePath))!;
+	}
+	
+	// Check {save.json} if exist
+	// filePath - [string]
+	// false -> create new {filePath} file
+	private void FileExist(string filePath)
+	{
+		try
+		{
+			JsonConvert.DeserializeObject(File.ReadAllText(filePath));
+		}
+		catch
+		{
+			File.Create(filePath).Close();
+			File.WriteAllText(filePath, "{}");
+		}
+	}
+}
